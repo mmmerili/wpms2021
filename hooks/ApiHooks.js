e857af4aefcc527a4c4d4a6261f1961939146ae1
mmmerili <meriliru@metropolia.fi>
2021-08-24 13:50:48 +0300

    lisää muutoksia c

--- hooks/ApiHooks.js
+++ hooks/ApiHooks.js
@@ -1,28 +1,36 @@
-// TODO: add necessary imports
-import {baseUrl} from '../utils/variables';
 import {useEffect, useState} from 'react';
+import {baseUrl} from '../utils/variables';
 
 const useMedia = () => {
   const [mediaArray, setMediaArray] = useState([]);
 
   useEffect(() => {
-    const loadMedia = async () => {
-      try {
-        const response = await fetch(baseUrl + 'media');
-        const mediaIlmanThumbnailia = await response.json();
-        const kaikkitiedot = mediaIlmanThumbnailia.map(async (media) => {
-          const response = await fetch(baseUrl + 'media/' + media.file_id);
-          const tiedosto = await response.json();
-          return tiedosto;
-        });
-        setMediaArray(await Promise.all(kaikkitiedot));
-      } catch (e) {
-        console.log(e.message());
-      }
-    };
-    loadMedia();
+    // https://scriptverse.academy/tutorials/js-self-invoking-functions.html
+    (async () => {
+      setMediaArray(await loadMedia());
+    })();
   }, []);
-  return {mediaArray};
+
+  const loadMedia = async () => {
+    try {
+      const response = await fetch(baseUrl + 'media');
+      const mediaIlmanThumbnailia = await response.json();
+      const kaikkiTiedot = mediaIlmanThumbnailia.map(async (media) => {
+        return await loadSingleMedia(media.file_id);
+      });
+      return Promise.all(kaikkiTiedot);
+    } catch (e) {
+      console.log(e.message());
+    }
+  };
+
+  const loadSingleMedia = async (id) => {
+    const response = await fetch(baseUrl + 'media/' + id);
+    const tiedosto = await response.json();
+    return tiedosto;
+  };
+
+  return {mediaArray, loadMedia, loadSingleMedia};
 };
 
 export {useMedia};
