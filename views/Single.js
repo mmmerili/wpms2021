157b7a572708229182f2d6d961b04ad23641cd0d
mmmerili <meriliru@metropolia.fi>
2021-08-30 14:20:02 +0300

    navigation stuff

---
+++ views/Single.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, SafeAreaView, Text, Image} from 'react-native';
+import {uploadsUrl} from '../utils/variables';
+import {format} from 'date-fns';
+
+const Single = ({route}) => {
+  const {params} = route;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>{params.title}</Text>
+      <Image
+        style={{width: 200, height: 200}}
+        source={{uri: uploadsUrl + params.filename}}
+      />
+      <Text>{params.description}</Text>
+      <Text>{params.user_id}</Text>
+      <Text>{format(new Date(params.time_added), 'dd.MM.yyyy')}</Text>
+      <Text>{params.media_type}</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 40,
+  },
+});
+
+Single.propTypes = {
+  route: PropTypes.object.isRequired,
+};
+
+export default Single;
