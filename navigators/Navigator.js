a2ee8af04af5717db99cdfbb0ebe79cd902910d5
mmmerili <meriliru@metropolia.fi>
2021-08-31 23:24:11 +0300

    login changes

--- navigators/Navigator.js
+++ navigators/Navigator.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
-import Home from '../views/Home';
 import Profile from '../views/Profile';
+import Home from '../views/Home';
 import Single from '../views/Single';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Login from '../views/Login';
+import {MainContext} from '../contexts/MainContext';
 
 const Tab = createBottomTabNavigator();
+
 const Stack = createNativeStackNavigator();
 
 const TabScreen = () => {
   return (
-    // TODO: move content of <NavigationContainer> here
     <Tab.Navigator>
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Profile" component={Profile} />
@@ -20,10 +22,17 @@ const TabScreen = () => {
 };
 
 const StackScreen = () => {
+  const {isLoggedIn} = useContext(MainContext);
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Front" component={TabScreen} />
-      <Stack.Screen name="Single" component={Single} />
+      {isLoggedIn ? (
+        <>
+          <Stack.Screen name="FrontHome" component={TabScreen} />
+          <Stack.Screen name="Single" component={Single} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={Login} />
+      )}
     </Stack.Navigator>
   );
 };
@@ -36,4 +45,4 @@ const Navigator = () => {
   );
 };
 
-export default Navigator;
+export default Navigator
--- views/Profile.js
+++ views/Profile.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
+import {MainContext} from '../contexts/MainContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = (props) => {
+  const {isLoggedIn, setIsLoggedIn} = useContext(MainContext);
+  console.log('profile', isLoggedIn);
+
+  const logout = async () => {
+    setIsLoggedIn(false);
+    await AsyncStorage.clear();
+    props.navigation.navigate('Login');
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text>Profile</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Login</Text>
+      <Button title="Log out!" onPress={logout} />
+    </View>
   );
 };
 
